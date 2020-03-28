2e77ee76b7e396a26a988e4c047317b6261e9679
Ma7eer <maher.alkendi2@gmail.com>
2020-03-28 19:10:13 +0400

    added compression

--- server/app.js
+++ server/app.js
@@ -2,15 +2,26 @@ import createError from 'http-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import compression from 'compression';
 
+/**
+ * routes.
+ */
 import indexRouter from './routes/index';
 
+/**
+ * Create Express server.
+ */
 const app = express();
 
+/**
+ * Express configuration.
+ */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 
 app.use('/', indexRouter);
 
