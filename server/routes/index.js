248fc084de7b39f13e5499bbb11cbfa425c97f94
Ma7eer <maher.alkendi2@gmail.com>
2020-03-28 17:37:02 +0400

    added prettier

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res) {
-  res.status(200).json({ message: "Hello World" });
+router.get('/', function (req, res) {
+  res.status(200).json({ message: 'Hello World' });
 });
 
 export default router;
